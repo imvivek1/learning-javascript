b99c379a9c54a5b38ab3c427a5aa55d842e7784e
Vivek <vivek61504@gmail.com>
2024-04-05 23:37:05 +0530

    dataype

--- 05_dataType.js
+++ 05_dataType.js
@@ -1,9 +1,34 @@
 // #Primitive
-  //7 type:String,Number, Boolean, null, undefined, Symbol, BigInt
+//7 type:String,Number, Boolean, null, undefined, Symbol, BigInt
 
 
+// const score=100;
+// const scoreValue=100.3;
+
+// const IsLoggedIn=false;
+// const outsideTemp=null;//null means null
+// let userEmail;//undefined
 
 
+// const id= Symbol('123');
+// const anotherId= Symbol('123');
+
+// console.log(id===anotherId);
+// const bigNumber = 614368683773
 
 //non-primtve or refrence type
- //Array, Objects, 
+ //Array, Objects, Functions
+
+// const hero=["spiderman", "captain","thor"];
+// let myObj={
+//   name:"vivek",
+//   age:21,
+
+// }
+
+// console.log(myObj);
+let MyFun=function()
+{
+  console.log("hello World");
+}
+MyFun();
