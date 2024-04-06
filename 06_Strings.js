ef88940fc8cd8f85e3336c986e5ab95606873095
Vivek <vivek61504@gmail.com>
2024-04-07 00:51:11 +0530

    Strings

--- 06_Strings.js
+++ 06_Strings.js
@@ -15,6 +15,14 @@ const gameName= new String('vivekkr')
 // console.log(gameName.charAt(3));// for finding index no. of character
 // console.log(gameName.indexOf('k'));//for finding index of character
 
-const newString = gameName.substring(0,4);
-console.log(newString);
 
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7, 5);
+// console.log(anotherString);
+
+const url="https://vivek.com/vivek%20kumar";//%20 means browser will automatically convert spacing in %20
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('anisul'));// this function checks anisul is present in url or not
