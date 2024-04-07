d1f3cf4a6ba530ede1acfcb164fbb84e1b080bfc
Vivek <vivek61504@gmail.com>
2024-04-07 16:21:42 +0530

    strins

--- 07_NumberAndMaths.js
+++ 07_NumberAndMaths.js
@@ -15,3 +15,24 @@ const balance=new Number(100);// ouput [Number: 100] speciafically defined objec
 
 
 ////////////////////////////////////////////////////////////////////////////////
+
+// console.log(Math);
+// console.log(Math.abs(-4));// absolute value means convert negative value to positive
+
+// console.log(Math.round(4.6)); //it will choose nearest non decimal number
+
+// console.log(Math.ceil(4.3));//it will choose nearest non decimal highest number
+// console.log(Math.floor(4.9));//it will choose nearest non decimal lowest number
+
+// console.log(Math.max(1,5,8,3,7,9));//to find max number
+// console.log(Math.min(1,5,8,3,7,9));//to find minimum number
+
+// console.log(Math.random());//random value will always comes between 0 and 1s
+
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=1;
+const max=6;
+
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
