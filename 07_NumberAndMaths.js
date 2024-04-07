f99042bec09d87d27972aa5d264fda0fc32d7ec4
Vivek <vivek61504@gmail.com>
2024-04-07 15:53:04 +0530

    numbers

---
+++ 07_NumberAndMaths.js
@@ -0,0 +1,17 @@
+const score=500;
+//  console.log(score);
+
+const balance=new Number(100);// ouput [Number: 100] speciafically defined object
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//precision value no. of digits after decimals
+
+// const otherNumber =123.8966;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds=10000000;
+// console.log(hundreds.toLocaleString());//number system comas(,)
+
+
+////////////////////////////////////////////////////////////////////////////////
